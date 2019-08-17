a99cf117e68a0c6826d136e00ced1430721e1d49
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-08-17 16:52:02 +0300

    Add files via upload

---
+++ ProductManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FileStream_Homework
+{
+    class ProductManager
+    {
+        public List<Product> _collection;
+        private string path;
+        public ProductManager()
+        {
+            path = @"C:\Users\user\Desktop\MyJson.Json";
+            LoadCollection();
+            //_collection = new List<Product>();
+
+        }
+        //Private Methods
+        private void LoadCollection()
+        {
+            Stream readingStream = new FileStream(path, FileMode.OpenOrCreate);
+            try
+            {
+                byte[] temp = new byte[100];
+                int len = 0;
+                string jSon = "";
+
+                while ((len = readingStream.Read(temp, 0, temp.Length)) > 0)
+                {
+                    jSon += Encoding.UTF8.GetString(temp, 0, len);
+                }
+                _collection = JsonConvert.DeserializeObject<List<Product>>(jSon);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                readingStream.Close();
+            }
+        }
+
+        private void SaveCollection()
+        {
+            Stream writingStream = new FileStream(path, FileMode.Create);
+            try
+            {
+                string jSon = JsonConvert.SerializeObject(_collection);
+                byte[] bytes = Encoding.ASCII.GetBytes(jSon);
+
+                writingStream.Write(bytes, 0, bytes.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writingStream.Close();
+            }
+        }
+
+        //Public Methods
+        public void AddItem()
+        {
+            Product newItem = new Product();
+            Console.Write("Id: ");
+            newItem.Id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            newItem.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            newItem.Price = Console.ReadLine();
+            _collection.Add(newItem);
+            SaveCollection();
+            Console.WriteLine("Item added!");
+        }
+
+        public void RemoveItem(int index)
+        {
+            if(index < _collection.Count)
+            {
+                _collection.RemoveAt(index);
+                SaveCollection();
+                Console.WriteLine("Item removed!");
+            }
+            else
+            {
+                Console.WriteLine("NO ITEM FOUND");
+            }
+        }
+
+        public void UpdateItem(int index)
+        {
+            if (index < _collection.Count)
+            {
+                Console.Write("Id: ");
+                _collection[index].Id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                _collection[index].Name = Console.ReadLine();
+                Console.Write("Price: ");
+                _collection[index].Price = Console.ReadLine();
+                SaveCollection();
+                Console.WriteLine("Item updated!");
+            }
+            else
+            {
+                Console.WriteLine("NO ITEM FOUND");
+            }
+        }
+
+        public void ClearCollection()
+        {
+            for(int i = 0;i < _collection.Count; i++)
+            {
+                _collection.RemoveAt(0);
+                i--;
+            }
+            SaveCollection();
+            Console.WriteLine("Items Cleared!");
+        }
+    }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStream_Homework
+{
+    class UserInterface
+    {
+        private bool _isShown = true;
+        ProductManager pMan = new ProductManager();
+
+        public void StartInterface()
+        {
+            
+
+            while(_isShown == true)
+            {
+                ShowInterface();
+            }
+            Console.WriteLine("Goodbye");
+        }
+
+        public void ShowCollection()
+        {
+            Console.WriteLine("collection: ");
+            Console.WriteLine("----------");
+            if (pMan._collection.Count > 0)
+            {
+                for (int i = 0; i < pMan._collection.Count; i++)
+                {
+                    Console.WriteLine("----------");
+                    Console.WriteLine("Id: " + pMan._collection[i].Id);
+                    Console.WriteLine("Name: " + pMan._collection[i].Name);
+                    Console.WriteLine("Price: " + pMan._collection[i].Price);
+                    Console.WriteLine("----------");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No items were found!");
+            }
+        }
+
+        public void ShowInterface()
+        {
+            Console.WriteLine("----------");
+            Console.WriteLine("Please select an option: ");
+            Console.WriteLine("1 - Add a product");
+            Console.WriteLine("2 - Update a product");
+            Console.WriteLine("3 - Remove a product");
+            Console.WriteLine("4 - Show all products");
+            Console.WriteLine("5 - Clear all products");
+            Console.WriteLine("6 - Exit");
+
+            string option = Console.ReadLine();
+
+            if(option == "1") //Add
+            {
+                pMan.AddItem();
+            }
+            if (option == "2") //Update
+            {
+                Console.Write("Enter index: ");
+                int index = int.Parse(Console.ReadLine());
+                pMan.UpdateItem(index);
+            }
+            if (option == "3") //Remove
+            {
+                Console.Write("Enter index: ");
+                int index = int.Parse(Console.ReadLine());
+                pMan.RemoveItem(index);
+            }
+            if (option == "4") //Show
+            {
+                ShowCollection();
+            }
+            if(option == "5")
+            {
+                pMan.ClearCollection();
+            }
+            if (option == "6") //Exit
+            {
+                _isShown = false;
+            }
+        }
+    }
+}
